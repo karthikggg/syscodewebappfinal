1e60e6f6efd9116c7e87d2929fc02b07ab2657d8
karthikggg <karthik24003@gmail.com>
2024-07-31 02:19:18 +0530

    Updates

---
+++ precache-manifest.603e6ef621af59e1af10108acbb2195e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dccddb620d27e539ac4c43319496aa3c",
+    "url": "/sampledeployone/index.html"
+  },
+  {
+    "revision": "b95a1f61fbe19d678aec",
+    "url": "/sampledeployone/static/css/main.f6c1c810.chunk.css"
+  },
+  {
+    "revision": "e4594e9d62eb071816d5",
+    "url": "/sampledeployone/static/js/2.fb258b08.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/sampledeployone/static/js/2.fb258b08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f30efe28f1d09b78f7d",
+    "url": "/sampledeployone/static/js/3.0d31e61e.chunk.js"
+  },
+  {
+    "revision": "b95a1f61fbe19d678aec",
+    "url": "/sampledeployone/static/js/main.56e7a8d1.chunk.js"
+  },
+  {
+    "revision": "b8013b6824fc6b245b07",
+    "url": "/sampledeployone/static/js/runtime-main.846a6ec4.js"
+  }
+]);
